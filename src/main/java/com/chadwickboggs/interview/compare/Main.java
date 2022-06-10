bb5594bbf715f81c16d6d9b4d65004d4e9e3afa5
Tiffany Timbric <tiffanytimbric@gmail.com>
2022-06-10 09:39:11 -0600

    Added list compare/find_duplicates interview code.

---
+++ src/main/java/com/chadwickboggs/interview/compare/Main.java
@@ -0,0 +1,21 @@
+package com.chadwickboggs.interview.compare;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    public static void main( String... args ) {
+        List<Integer> listOne = Arrays.asList( 2, 3, 4 );
+        List<Integer> listTwo = Arrays.asList( 4, 9, 7 );
+
+        List<Integer> intersection = listOne.stream()
+            .filter( value -> listTwo.contains( value ) )
+            .collect( Collectors.toList() );
+
+        System.out.println( intersection );
+    }
+
+}
